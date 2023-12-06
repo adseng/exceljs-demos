9ba4b960f7e740b4503b269fb52b25ed6d43eb56
huang zhaohu <1983902539@qq.com>
2023-12-06 16:18:34 +0800

    webpack 把打包后资源内联进html里面

--- src/demo06/index.js
+++ src/demo06/index.js
@@ -21,7 +21,7 @@ const List16 = [
     '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
     '11', '12', '13', '14', '15', '16'
 ]
-
+console.log('sssaaa')
 function selected(c1, c2) {
     const haomaList = document.querySelectorAll(c1);
     haomaList.forEach(item => {
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,33 +2,38 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin')
 
-module.exports = {
-    entry: './demo06/index.js',  // 入口文件路径
-    output: {
-        filename: 'bundle.js',  // 输出文件名
-        path: path.resolve(__dirname, 'dist')  // 输出目录路径
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './demo06/index.html', // 指定HTML模板文件
-            filename: 'index.html', // 指定输出的HTML文件名
-            inject: 'body', // 默认值：true。确定脚本和样式的位置，可以设置为"head"或"body"
-            // inlineSource: '.(js|css)$', // 这里我会注入所有的css和js到html中，也可以根据需求修改正则，只注入部分文件，实测没有用
-            minify: { // 可选：压缩HTML文件
-                collapseWhitespace: true,
-                removeComments: true,
-                removeRedundantAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                minifyJS: true,
-                minifyCSS: true
-            }
-        }),
-        new HtmlInlineScriptPlugin()
-    ],
-    devServer: {
-        static: './dist',
-        open: true,  // 自动打开浏览器
-        port: 8080,  // 端口号
-        hot: true  // 热更新
+module.exports = (env, argv) => {
+
+    const mode = argv.mode || 'development'
+
+    return {
+        entry: './demo06/index.js',  // 入口文件路径
+        output: {
+            filename: 'bundle.js',  // 输出文件名
+            path: path.resolve(__dirname, 'dist')  // 输出目录路径
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './demo06/index.html', // 指定HTML模板文件
+                filename: 'index.html', // 指定输出的HTML文件名
+                inject: 'body', // 默认值：true。确定脚本和样式的位置，可以设置为"head"或"body"
+                // inlineSource: '.(js|css)$', // 这里我会注入所有的css和js到html中，也可以根据需求修改正则，只注入部分文件，实测没有用
+                minify: { // 可选：压缩HTML文件
+                    collapseWhitespace: true,
+                    removeComments: true,
+                    removeRedundantAttributes: true,
+                    removeStyleLinkTypeAttributes: true,
+                    minifyJS: true,
+                    minifyCSS: true
+                }
+            }),
+            mode === 'production' ? new HtmlInlineScriptPlugin() : ''
+        ],
+        devServer: {
+            static: './dist',
+            open: true,  // 自动打开浏览器
+            port: 8080,  // 端口号
+            hot: true  // 热更新
+        }
     }
 }
