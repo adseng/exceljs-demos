9b012caef82ca77ba72351b7b476b841aaa0baab
huang zhaohu <1983902539@qq.com>
2023-12-07 10:20:47 +0800

    tenser plugin

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+
 
 module.exports = (env, argv) => {
 
@@ -12,9 +14,25 @@ module.exports = (env, argv) => {
             filename: 'bundle.js',  // 输出文件名
             path: path.resolve(__dirname, 'dist')  // 输出目录路径
         },
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    format: {
+                        comments: false
+                    },
+                    terserOptions: {
+                        compress: {
+                            drop_console: true,
+                            drop_debugger: true
+                        }
+                    }
+                }),
+            ]
+        },
         plugins: [
             new HtmlWebpackPlugin({
-                template: './demo06/index.html', // 指定HTML模板文件
+                // template: './demo06/index.html', // 指定HTML模板文件
                 filename: 'index.html', // 指定输出的HTML文件名
                 inject: 'body', // 默认值：true。确定脚本和样式的位置，可以设置为"head"或"body"
                 // inlineSource: '.(js|css)$', // 这里我会注入所有的css和js到html中，也可以根据需求修改正则，只注入部分文件，实测没有用
@@ -26,8 +44,8 @@ module.exports = (env, argv) => {
                     minifyJS: true,
                     minifyCSS: true
                 }
-            }),
-            mode === 'production' ? new HtmlInlineScriptPlugin() : ''
+            })
+            // mode === 'production' ? new HtmlInlineScriptPlugin() : ''
         ],
         devServer: {
             static: './dist',
